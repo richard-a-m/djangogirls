d03cf61808114fa927c0196dd9be614b2cf468bc
rxm747 <rxm747@cs.bham.ac.uk>
2020-08-14 03:12:33 +0100

    Setup

---
+++ dgev/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\richard\eclipse-workspace\djangogirls\dgev\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
